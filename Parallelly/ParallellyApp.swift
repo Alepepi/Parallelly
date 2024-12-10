f2302cfc8d5b71a692be39e1b14b658c5e42caeb
Alejandro Ollivier <aleollivierochoa@gmail.com>
2024-12-10 17:11:00 +0100

    Initial Commit

---
+++ Parallelly/ParallellyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallellyApp.swift
+//  Parallelly
+//
+//  Created by Alejandro Ollivier Ochoa on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ParallellyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
